20c0a5ae6c2ce5315a24671ff534f81da88ccc07
JAIDHEER SIRIGINEEDI <72718607+JAIDHEER007@users.noreply.github.com>
2021-12-09 10:20:46 +0530

    Combinations_Recursive_C++

---
+++ Programs/LC 77/LC_77_1.cpp
@@ -0,0 +1,48 @@
+// Runtime: 436ms
+// FT: 15.99%
+// Memory Usage: 164.4 mb
+// LT: 12.18%
+
+#include<iostream>
+#include<vector>
+using namespace std; 
+
+void findCombinations(vector<vector<int>> &result,
+					  int &n, int &k,
+					  vector<int> arr = {},
+					  int currNum = 1){
+
+	// Base Case
+	if(arr.size() == k){
+		result.push_back(arr); 
+		return;
+	}	
+	
+	// Recursive Case	
+	for(int i = currNum; i <= n; ++i){
+		arr.push_back(i);
+		findCombinations(result, n, k, arr, (i + 1));
+		arr.pop_back(); 
+	}			  	
+}
+
+vector<vector<int>> combinations(int n, int k){
+	vector<vector<int>> result; 
+	findCombinations(result, n, k);
+	return result; 
+}
+
+int main(){
+	int n, k;
+	cin>>n>>k; 
+	
+	vector<vector<int>> result = combinations(n, k); 
+	
+	for(vector<int> row:result){
+		for(int element:row)
+			cout<<element<<" ";
+		cout<<endl; 
+	}
+	
+	return 0; 
+}
