b367f5248ab41daf6922e90e3878f119429147be
jaidheer <jd8331842095@gmail.com>
2022-02-04 22:47:33 +0530

    nothing

--- createFolder1.bat
+++ createFolder1.bat
@@ -9,6 +9,11 @@ if not exist "%cd%\Programs\LC %QNUM%" (
     echo Folder not present
     echo Creating new folder at "%cd%\Programs\LC %QNUM%"
     mkdir "%cd%\Programs\LC %QNUM%" 
+    echo Enter Test Case Here > "%cd%\Programs\LC %QNUM%"\input1.txt
+    echo Enter Test Case Here > "%cd%\Programs\LC %QNUM%"\input2.txt
+    echo Enter Test Case Here > "%cd%\Programs\LC %QNUM%"\input3.txt
+    echo Enter Test Case Here > "%cd%\Programs\LC %QNUM%"\input4.txt
+    echo Enter Test Case Here > "%cd%\Programs\LC %QNUM%"\input5.txt
 ) else (
     echo Folder already present
 )
