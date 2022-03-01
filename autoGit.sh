23b1815c5e1fa6e0482ae9f55a344ff2ab1d3773
jaidheer <jd8331842095@gmail.com>
2022-03-01 10:00:58 +0530

    Added autoGit.sh

--- autoGit.sh
+++ autoGit.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Read the file path
 echo Enter Relative file path?
 read -r fpath
 
+# Extract the direcotry / file name from the file path
 name=$(basename "$fpath")
+
+# Use the name to create the commit message
 commitMsg="Added $name"
 
 # echo $name
